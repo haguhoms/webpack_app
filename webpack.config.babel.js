6cd3bca36fc225e23e344a23bfe30972b3e845fc
haguhoms <haguhoms@gmail.com>
2017-05-16 19:32:38 +0900

    react導入

---
+++ webpack.config.babel.js
@@ -0,0 +1,63 @@
+import webpack from 'webpack'
+
+require('babel-polyfill');
+
+module.exports = [
+    {
+        entry: './src/js/app.js',
+        output: {
+            path: __dirname + '/bin/js',
+            filename: 'app.bundle.js'
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.js?$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader'
+                },
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader'
+                },
+                {
+                    test: /\.json?$/,
+                    loader: 'json-loader'
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: function() {
+                                    return [
+                                        require('precss'),
+                                        require('autoprefixer')
+                                    ]
+                                }
+                            }
+                        },
+                        'sass-loader'
+                    ]
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['*', '.js', '.jsx', '.json']
+        },
+        plugins: [
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false,
+                },
+                output: {
+                    comments: false,
+                },
+            }),
+        ]
+    }
+];
